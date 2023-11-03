8f28c4e913dec36ab482a2310950458ffb0378fe
andrepresys <andre.presys@gmail.com>
2023-11-03 16:37:25 -0300

    Recebimento de dados na controller de Automações

--- FESA.EDU.ECOLIGHT.WEB.FRONTEND/Controllers/AutomacoesController.cs
+++ FESA.EDU.ECOLIGHT.WEB.FRONTEND/Controllers/AutomacoesController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using FESA.EDU.ECOLIGHT.WEB.FRONTEND.Models.Automacao;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FESA.EDU.Ecolight.Web.FRONTEND.Controllers
@@ -22,9 +23,19 @@ namespace FESA.EDU.Ecolight.Web.FRONTEND.Controllers
             return View();
         }
 
+        public IActionResult Cadastrar(AutomacaoViewModel viewModel)
+        {
+            return View("Index");
+        }
+
         public IActionResult Detalhes()
         {
             return View();
+        }
+
+        public IActionResult Editar(AutomacaoViewModel viewModel)
+        {
+            return View("Detalhes");
         }
     }
 }
---
+++ FESA.EDU.ECOLIGHT.WEB.FRONTEND/Models/Automacao/AutomacaoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace FESA.EDU.ECOLIGHT.WEB.FRONTEND.Models.Automacao
+{
+    public class AutomacaoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public TimeOnly Inicio { get; set; }
+        public TimeOnly Fim { get; set; }
+        public int Brilho { get; set; }
+        public int DispositivoId { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
